9b49797308a3dedc1a01b72332210ca7caa5c642
Logan Woodbury <testare@byui.edu>
2016-02-15 10:31:26 -0700

    Initial commit

---
+++ app/src/main/java/edu/byu/testare/cs246multi_threadedprogramming/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.byu.testare.cs246multi_threadedprogramming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
